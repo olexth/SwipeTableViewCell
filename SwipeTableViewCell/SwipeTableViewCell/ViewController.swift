2e90b65468c7e13384940cd70d2ff6f1cf8962a4
Alex Golub <alexeythumb@gmail.com>
2019-02-26 20:42:08 +0200

    Add Xcode project

---
+++ SwipeTableViewCell/SwipeTableViewCell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwipeTableViewCell
+//
+//  Created by Alex Golub on 2/26/19.
+//  Copyright © 2019 Alex Golub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
